c4bc438e00a4a9b4ce1d1887d3991fd7dd1e2ada
Evgeniy <evgnec@gmail.com>
2025-06-06 01:02:42 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
+import initialTodos from './todos.json'
 
-export class App extends Component {
+class App extends Component {
+  state = {
+    todos: initialTodos,
+    filter:'',
+  };
+
+test = () => {}
+
+changeStatus = (id) => {
+  this.setState(prevState =>({
+    todos: prevState.todos.map(todo => {
+if (todo.id === id){
+        return {...todo, completed: !todo.completed}
+      }
+      return todo
+    })
+  }))   
+};
+
   render() {
+    const {todos} = this.state;
     return (
-      <div>
-        
-      </div>
+      <>
+      <ul>
+      {todos.map(({id, title, completed}) =>
+      (
+        <li key={id}>
+          <p>{title}</p>
+          <input 
+						type='checkbox'
+						checked={completed}
+						onChange={() => this.changeStatus(id)}
+					/>
+        </li>
+      )
+      )
+      }      
+      </ul>
+				
+      </>
     )
   }
 }
 
-export default App
+export default App;
+
+/* <div >
+
+				</div>
+				<div >
+					<button
+						type='button'
+						aria-label='Close'
+						onClick={() => this.test}
+					></button>
+				</div> */
 
 
 
